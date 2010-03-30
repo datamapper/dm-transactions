6fb9d54413ce74c547b2f7824c179b90e7658ab3
snusnu <gamsnjaga@gmail.com>
2010-03-30 19:22:35 +0200

    Initial commit to dm-transactions.

---
+++ Rakefile
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'rake'
+
+begin
+
+  require 'jeweler'
+
+  Jeweler::Tasks.new do |gem|
+    gem.name        = "dm-transactions"
+    gem.summary     = %Q{Adds transaction support to datamapper}
+    gem.description = %Q{Makes transaction support available for adapters that support them}
+    gem.email       = "gamsnjaga@gmail.com"
+    gem.homepage    = "http://github.com/datamapper/dm-transactions"
+    gem.authors     = ["Dirkjan Bussink (dbussink)", "Dan Kubb (dkubb)"]
+
+    gem.add_dependency             'dm-core', '~> 0.10.2'
+    gem.add_development_dependency 'rspec',   '~> 1.3'
+
+  end
+
+  Jeweler::GemcutterTasks.new
+
+  FileList['tasks/**/*.rake'].each { |task| import task }
+  
+rescue LoadError
+  puts "Jeweler (or a dependency) not available. Install it with: gem install jeweler"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,26 @@
+require 'dm-transactions'
+
+require 'dm-core/spec/lib/counter_adapter'
+require 'dm-core/spec/lib/pending_helpers'
+require 'dm-core/spec/lib/adapter_helpers'
+
+require 'dm-core/spec/resource_shared_spec'
+require 'dm-core/spec/sel_shared_spec'
+
+ENV['ADAPTERS'] ||= 'sqlite3'
+
+# create sqlite3_fs directory if it doesn't exist
+temp_db_dir = Pathname(File.expand_path('../db', __FILE__))
+temp_db_dir.mkpath
+
+DataMapper::Spec::AdapterHelpers.temp_db_dir = temp_db_dir
+
+adapters  = ENV['ADAPTERS'].split(' ').map { |adapter_name| adapter_name.strip.downcase }.uniq
+adapters  = DataMapper::Spec::AdapterHelpers.primary_adapters.keys if adapters.include?('all')
+
+DataMapper::Spec::AdapterHelpers.setup_adapters(adapters)
+
+Spec::Runner.configure do |config|
+  config.extend(DataMapper::Spec::AdapterHelpers)
+  config.include(DataMapper::Spec::PendingHelpers)
+end
