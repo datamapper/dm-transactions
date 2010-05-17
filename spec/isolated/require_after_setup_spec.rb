ffe46996e211970144636c31b950bfc697cc79f7
Dan Kubb <dan.kubb@gmail.com>
2010-05-17 16:40:29 -0700

    Added adapter guard clauses to isolation specs

--- spec/isolated/require_after_setup_spec.rb
+++ spec/isolated/require_after_setup_spec.rb
@@ -1,17 +1,21 @@
 require 'spec'
 require 'isolated/require_spec'
 require 'dm-core/spec/setup'
+require 'dm-core/spec/lib/adapter_helpers'
 
 # To really test this behavior, this spec needs to be run in isolation and not
 # as part of the typical rake spec run, which requires dm-transactions upfront
 
 describe "require 'dm-transactions after calling DataMapper.setup" do
+  extend DataMapper::Spec::Adapters::Helpers
 
   before(:all) do
     @adapter = DataMapper::Spec.adapter
     require 'dm-transactions'
   end
 
-  it_should_behave_like "require 'dm-transactions'"
+  supported_by :postgres, :mysql, :sqlite, :oracle, :sqlserver do
+    it_should_behave_like "require 'dm-transactions'"
+  end
 
 end
