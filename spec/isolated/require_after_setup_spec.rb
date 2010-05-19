ec23600f8a3f673a8865f1bbfb79aa2646ac1019
snusnu <gamsnjaga@gmail.com>
2010-05-19 04:59:04 +0200

    Don't execute any isolated specs on inappropriate adapters
    We definitely need a better solution to run some
    specs in isolation (automated within the CI process)
    but this will do for now. It makes rake spec pass
    on all adapters on all rubies.

--- spec/isolated/require_after_setup_spec.rb
+++ spec/isolated/require_after_setup_spec.rb
@@ -6,16 +6,18 @@ require 'dm-core/spec/lib/adapter_helpers'
 # To really test this behavior, this spec needs to be run in isolation and not
 # as part of the typical rake spec run, which requires dm-transactions upfront
 
-describe "require 'dm-transactions after calling DataMapper.setup" do
-  extend DataMapper::Spec::Adapters::Helpers
+if %w[postgres mysql sqlite oracle sqlserver].include?(ENV['ADAPTER'])
 
-  before(:all) do
-    @adapter = DataMapper::Spec.adapter
-    require 'dm-transactions'
-  end
+  describe "require 'dm-transactions after calling DataMapper.setup" do
+    extend DataMapper::Spec::Adapters::Helpers
 
-  supported_by :postgres, :mysql, :sqlite, :oracle, :sqlserver do
+    before(:all) do
+      @adapter = DataMapper::Spec.adapter
+      require 'dm-transactions'
+    end
+
     it_should_behave_like "require 'dm-transactions'"
+
   end
 
 end
